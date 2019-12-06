fb4f71007ba4b689ebbb986df74c9a9ac1f82af0
brand <brandonmarion@billericak12.com>
2019-12-06 11:33:18 -0500

    gas leaking from the rubber

--- src/com/company/Factorial.java
+++ src/com/company/Factorial.java
@@ -2,8 +2,27 @@ package com.company;
 
 public class Factorial {
 
-    public static void calcFactorial() {
-        
+    public static long calcFactorial(int a) {
+        long b = 1;
+        while (a > 0) {
+            b = b * a;
+            a--;
+        }
+        return b;
     }
+
+    public static int calcE(int a){
+        int b=1;
+        while(a=) {
+            b = b/a;
+
+    }
+
+        public static void main (String[]args) {
+            for (int g = 1; g <= 20; g++) {
+                System.out.println(calcFactorial(g));
+
+            }
+        }
 
 }
