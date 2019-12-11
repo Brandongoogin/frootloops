fa8995f91b174eadb754c78d7babc9f5f2769c54
brandon <brandonmarion@billericak12.com>
2019-12-11 11:46:02 -0500

    yyyyy

--- src/com/company/Factorial.java
+++ src/com/company/Factorial.java
@@ -11,21 +11,47 @@ public class Factorial {
         return b;
     }
 
-    public static int calcE(int a){
-        int b=1;
-        int n=b;
-        while(a>0) {
-            b = b*a;
-            n = 1/b;
+    public static double calcE(){
+        double e = 1.0;
+        double olde = 0.0;
+        int count = 1;
+        while ((e-olde) > 0.001)
+        {
+            olde = e;
+            e = e + 1.0/calcFactorial(count);
+            count++;
+        }
+        return e;
+    }
+
+    public static double calcEX(int x){
+        double e = 1.0;
+        double olde = 0.0;
+        int count = 1;
+        while ((e-olde) > 0.001)
+        {
+            olde = e;
+            e = e + Math.pow(x,count)/calcFactorial(count);
+            count++;
+        }
+        return e;
+    }
+
+    public static void main (String [] args){
+
+        for (int i=1; i<=20; i++){
+            System.out.print(calcFactorial(i));
+            System.out.println();
+        }
+        System.out.println(calcE());
+        System.out.printf("e is %2.3f \n", calcEX(1));
+        System.out.printf("e is %2.3f \n", calcEX(2));
+        System.out.printf("e is %2.3f \n", calcEX(3));
+        System.out.printf("e is %2.3f \n", calcEX(4));
+        System.out.printf("e is %2.3f \n", calcEX(5));
 
     }
 
-        public static void main (String[]args) {
-            for (int g = 1; g <= 20; g++) {
-                System.out.println(calcFactorial(g));
-                System.out.println(                                                                                                                                      calcE();
-
-            }
-        }
-
 }
+
+
